1453b3f99203bc07da139c2d7438c904c8194ba0
dthang29 <thangbdhe180731@fpt.edu.vn>
2025-02-09 01:39:48 +0700

    Update Structure

---
+++ src/main/java/com/example/worldtravel/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.worldtravel.repository;
+
+import com.example.worldtravel.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+    User findByUsernameAndPassword(String username, String password);
+    User findByEmailAndPassword(String email, String password);
+}
